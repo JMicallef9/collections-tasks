a7b12aac9148c4e2c2aec1d3d44963760ad0cce1
Joseph Micallef <micallefje@gmail.com>
2024-12-02 11:07:04 +0000

    complete count_word_frequencies function with testing

--- src/count_word_frequencies.py
+++ src/count_word_frequencies.py
@@ -1,7 +1,29 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
+import re
 
 def count_word_frequencies(text):
+    """
+    Counts word frequencies in a given string.
+
+    Args:
+        text (str): A string of text.
+
+    Returns:
+        dict: A dictionary with frequency counts for each word in the string.
+    """
     word_freq = defaultdict(int)
     if text:
-        word_freq[text] += 1
+        words = text.lower().split()
+        for word in words:
+            word = re.sub('[!?.]', '', word)
+            word_freq[word] += 1 
     return word_freq
+
+
+# see alternative solution below using Counter class:
+
+# def count_word_frequencies(text):
+#     if not text:
+#         return Counter()
+#     words = re.findall(r'\b\w+\b', text.lower())
+#     return Counter(words)
--- test/test_count_word_frequencies.py
+++ test/test_count_word_frequencies.py
@@ -5,3 +5,16 @@ def test_empty_dict_returned_if_no_input():
 
 def test_counts_single_word_instance():
     assert count_word_frequencies('hello') == {'hello': 1}
+
+def test_counts_multiple_words():
+    assert count_word_frequencies('hello world') == {'hello': 1, 'world': 1}
+
+def test_counts_multiple_instances_of_same_word():
+    assert count_word_frequencies('hello world hello') == {'hello': 2, 'world': 1}
+
+def test_ignores_capital_letters():
+    assert count_word_frequencies('Hello world hello') == {'hello': 2, 'world': 1}
+
+def test_filters_out_punctuation_chars():
+    text = "The quick brown fox jumps over the lazy dog. The dog was not amused."
+    assert count_word_frequencies(text) == {'the': 3, 'quick': 1, 'brown': 1, 'fox': 1, 'jumps': 1, 'over': 1, 'lazy': 1, 'dog': 2, 'was': 1, 'not': 1, 'amused': 1}
