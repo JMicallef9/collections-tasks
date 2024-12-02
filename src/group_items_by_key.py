f2d3536a88c2e9188a6d4e885b82d98e40f10244
Joseph Micallef <micallefje@gmail.com>
2024-12-02 11:05:07 +0000

    add docstring for group_items_by_key.py

--- src/group_items_by_key.py
+++ src/group_items_by_key.py
@@ -1,6 +1,16 @@
 from collections import defaultdict
 
 def group_items_by_key(dict_list, key):
+    """
+    Groups items from a list of dictionaries by a given key.
+
+    Args:
+        dict_list (list): A list of dictionaries.
+        key (str): A key from the given dictionaries.
+
+    Returns:
+        dict: A new dictionary with items grouped by the specified key.
+    """
     try:
         new_dict = defaultdict(list)
         for dict in dict_list:
